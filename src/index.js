cd090bdd7d16a4ef396240d3e4b55a0951d4ab86
Albert Kogan <a.kogan@fursk.ru>
2019-03-18 19:56:02 +0200

    base tooltip added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
     BASE_LAYER
 } from './preview/constants';
 
-import { LINE_CHART_LAYER, X_AXIS_LAYER, LINES_LAYER } from './line-chart/constants';
+import { LINE_CHART_LAYER, X_AXIS_LAYER, LINES_LAYER, HOVERABLE_LAYER, TOOLTIP_LAYER } from './line-chart/constants';
 import Store from './common/store';
 import actions from './common/actions';
 import mutations from './common/mutations';
@@ -45,7 +45,6 @@ const store = new Store({
     state: INITIAL_STATE
 });
 
-console.log('store', store);
 window.chartStore = store;
 
 const chartWrapper = document.getElementById('chartWrapper');
@@ -53,7 +52,6 @@ const previewWrapper = document.createElement('div');
 const lineChartWrapper = document.createElement('div');
 
 const _data = processData(data);
-console.log('data', _data);
 
 const preview = new Preview({ width: 500, height: 100, store, data: _data[0] });
 const lineChart = new LineChart({ width: 500, height: 500, store, data: _data[0] });
@@ -64,6 +62,8 @@ lineChartWrapper.classList.add(styles.chartWrapper);
 lineChartWrapper.appendChild(lineChart.getLayer({ layerID: X_AXIS_LAYER }));
 lineChartWrapper.appendChild(lineChart.getLayer({ layerID: LINES_LAYER }));
 lineChartWrapper.appendChild(lineChart.getLayer({ layerID: LINE_CHART_LAYER }));
+lineChartWrapper.appendChild(lineChart.getLayer({ layerID: HOVERABLE_LAYER }));
+lineChartWrapper.appendChild(lineChart.getLayer({ layerID: TOOLTIP_LAYER }));
 
 previewWrapper.appendChild(preview.getLayer({ layerID: BASE_LAYER }));
 previewWrapper.appendChild(preview.getLayer({ layerID: WINDOW_LAYER }));
